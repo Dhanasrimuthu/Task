c37a7d5df92a22af2cb1cc8c6e73225779698d6f
Dhanasrimuthu <mdhanasri11062001@gamil.com>
2024-04-17 14:25:21 +0530

    modified files

--- js/jobsearch.js
+++ js/jobsearch.js
@@ -6,83 +6,75 @@ window.onload = function() {
         console.log("Allowing  to  access the jobsearch page.");
     }
 }
-// window.onload = function() {
-//     if (!checkCookies()) {
-        
-//         window.location.href = "./login.html";
-//     } else {
-        
-//         console.log("Cookies are present. Allowing access to the jobsearch page.");
-//     }
-// };
-
-// function checkCookies() {
-//     var cookies = document.cookie.split(';');
-//     var usernameFound = false;
-//     var passwordFound = false;
-//     for (var i = 0; i < cookies.length; i++) {
-//         var cookie = cookies[i].trim();
-//         if (cookie.startsWith("username=")) {
-//             usernameFound = true;
-//         }
-//         if (cookie.startsWith("password=")) {
-//             passwordFound = true;
-//         }
-//     }
-//     return usernameFound && passwordFound;
-// }
-
 // delete cookies when logout
 function funLogout(){
     document.cookie="username=;"+"expires="+new Date(2021,12,12).toUTCString();
     document.cookie="password=;"+"expires="+new Date(2021,12,12).toUTCString();
     window.location.href="../html/login.html";
 }
-
-
-// =============================================================
+// ==========
 let jobcount = document.getElementById("jobcount");
 let pageno = document.getElementById("pageno");
 pageno.textContent = 1;
 let divitem = document.querySelectorAll("#mainchild > div ");
 let count = divitem.length;
 const jobs = document.querySelectorAll('.item');
-console.log(jobs);
+// console.log(jobs);
+var btn=document.querySelectorAll(".btn");
+for(i=0;i<btn.length;i++){
+    btn[i].style.display="none";
+}
 // search
+var searchValue;
 var search;
-
-console.log(search);
+var searchCount=0;
+let shownJobs = new Set();
+console.log(shownJobs);
+// console.log(search);
 document.getElementById('search').addEventListener('input', function () {
-    const searchValue = this.value.trim().toLowerCase();
+    searchValue = this.value.trim().toLowerCase();
     search=searchValue;
+    searchCount=0;
+    shownJobs = new Set();
+//    array= searchFunction();
+searchFunction();
+});
+function searchFunction(){
     jobs.forEach(job => {
         const jobName = job.querySelector('.para1').textContent.toLowerCase();
         const companyName = job.querySelector('.para2').textContent.toLowerCase();
         // console.log(companyName);
         if (companyName.includes(searchValue) || jobName.includes(searchValue)) {
             job.style.display = 'flex';
+            searchCount=searchCount+1;
+            shownJobs.add(job); 
         } else {
             job.style.display = 'none';
         }
     });
+    console.log(shownJobs);
     jobcount.value = count;
     select=count;
     pageno.textContent = 1;
-});
-
+    // console.log(searchCount);
+}
 // empty the search bar
 function empty() {
+    search=undefined;
     document.getElementById('search').value = "";
+    searchCount=0;
     toShowAll(20);
+    shownJobs = new Set();
 }
-function toShowAll(select){
-    if (select == count) {
-              for (i = 1; i <= select; i++) {
+function toShowAll(){
+              for (i = 1; i <= count; i++) {
               document.getElementById("item" + i).style.display = "flex";
             }
             jobcount.value = count;
             pageno.textContent = 1;
-    }
+            for(i=0;i<btn.length;i++){
+                btn[i].style.display="none";
+            }
 }
 // ===================================================
 var benginindex,endindex,check,select;
@@ -90,63 +82,109 @@ var page=1;
 function pagefun(){
     select = Number(jobcount.value);
         //  To check how many times the next page will come
-        check = Math.ceil(count / select);
-        console.log(check);
-        benginindex=select*(page-1);
-        endindex=select*page-1;
+        page=1;
+        if(search == undefined || searchCount>select){
+            check = Math.ceil(count / select);
+            // console.log(check);
+            benginindex=select*(page-1);
+            endindex=select*page-1;
+        //    console.log(page);
+        if (select == count) {
+            toShowAll(select);
+            for(i=0;i<btn.length;i++){
+                btn[i].style.display="none"
+            }
+        }else{
+            for(i=0;i<btn.length;i++){
+                btn[i].style.display="";
+            }
+            toShowSelect();
+        }
+    }else{
+        searchByOrder();
+    }
+}
+var checkselect;
+function toShowSelect(){
+    if(shownJobs.size === 0){
         jobs.forEach((job,index)=>{
-            const jobName = job.querySelector('.para1').textContent.toLowerCase();
-            const companyName = job.querySelector('.para2').textContent.toLowerCase();
-            if(benginindex<=index && endindex>=index ){
-                if(search==null){
+            if(benginindex<=index && endindex>=index){
                     job.style.display="flex";
                 }else{
-                     if (companyName.includes(search) || jobName.includes(search)) {
-                    job.style.display = 'flex';
-                   }
-                }
-               
-            }else{
                 job.style.display="none";
             }
         });
-        pageno.textContent = page;
-       if(page < check){
+    }else{
+        const shownJobsArray = Array.from(shownJobs);
+    jobs.forEach(job => {
+    const index = shownJobsArray.indexOf(job);
+    if (index !== -1 && benginindex <= index && endindex >= index) {
+        job.style.display = "flex";
+    } else {
+        job.style.display = "none";
+    }
+});
+    }
+    pageno.textContent = page;
+    nextPageNo();
+}
+function nextPageNo(){
+    if(page < check){
         page++;
        }
-    //    console.log(page);
-    if (select == count) {
-        toShowAll(select);
-    }
+       benginindex=select*(page-1);
+       endindex=select*page-1;
 }
-
+function previousPageNo(){
+    if(page>1){
+        page--;
+       }
+       benginindex=select*(page-1);
+       endindex=select*page-1;
+}
+function pageFunNext(){
+    nextPageNo();
+    toShowSelect();
+}
 function pageFunPrevious(){
-   if(page>1){
-    page--;
-   }
-
-    benginindex=select*(page-1);
-    endindex=select*page-1;
+    previousPageNo();
+      if(shownJobs.size === 0){
         jobs.forEach((job,index)=>{
-            const jobName = job.querySelector('.para1').textContent.toLowerCase();
-            const companyName = job.querySelector('.para2').textContent.toLowerCase();
             if(benginindex<=index && endindex>=index ){
-                if(search==null){
                     job.style.display="flex";
                 }else{
-                     if (companyName.includes(search) || jobName.includes(search)) {
-                    job.style.display = 'flex';
-                   }
-                }
-            }else{
                 job.style.display="none";
             }
         })
+    }else{
+        const shownJobsArray = Array.from(shownJobs);
+        jobs.forEach(job => {
+        const index = shownJobsArray.indexOf(job);
+        if (index !== -1 && benginindex <= index && endindex >= index) {
+            job.style.display = "flex";
+        } else {
+            job.style.display = "none";
+        }
+    });
+    }
         pageno.textContent = page;
        page++;
        console.log(page); 
 }
 
-// =======================================================================
-
+function searchByOrder(){
+    if(searchCount<=select){
+        for(i=0;i<btn.length;i++){
+            btn[i].style.display="none"
+        }
+        console.log(searchCount)
+        searchCount=0;
+        jobcount.value = select;
+        searchFunction(); 
+    } else{
+        for(i=0;i<btn.length;i++){
+            btn[i].style.display=""
+        }
+    }
+}
 
